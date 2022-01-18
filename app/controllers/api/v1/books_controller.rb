097f86c5a8d8f676f087b9b4e1e276c5938a415e
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-18 11:15:59 -0700

    Feat - Add sad path for no quantity/location

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    render json: BookSerializer.new(BookFacade.new(params[:location], params[:quantity]))
+    if params[:location].present? && params[:quantity].present?
+      render json: BookSerializer.new(BookFacade.new(params[:location], params[:quantity]))
+    else
+      render json: "Please include a location and quantity", status: 400
+    end
   end
 end
