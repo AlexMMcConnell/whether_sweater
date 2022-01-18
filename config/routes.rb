c4c8658b41e842fb1214041469659237d1aa8cb6
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-17 19:48:23 -0700

    Feat - Add routes for all expected endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :forecast, only: [:index]
-      
+      resources :backgrounds, only: [:index]
+      resources :users, only: [:index]
+      resources :sessions, only: [:index]
+      resources :road_trip, only: [:index]
     end
   end
 end
