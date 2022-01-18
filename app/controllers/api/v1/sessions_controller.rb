69ce48e1e41eb97242c84450e6b538c5667022b8
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-17 20:35:19 -0700

    Refactor - Adjust sessions controller to properly find user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def index
     current_user = request.POST
-    user = User.find_by(current_user[:email])
+    user = User.find_by(email: current_user[:email])
     if user && (current_user[:password] == user[:password])
       render json: UserSerializer.new(user)
     else
