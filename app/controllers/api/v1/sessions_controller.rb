ce2dee934b56548c395156f11590647064183081
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-17 19:49:31 -0700

    Feat - Add sessions controller for user login

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::SessionsController < ApplicationController
+  def index
+    current_user = request.POST
+    user = User.find_by(current_user[:email])
+    if user && (current_user[:password] == user[:password])
+      render json: UserSerializer.new(user)
+    else
+      render json: "Invalid credentials", status: 400
+    end
+  end
+end
