95febe32447f5f78542ed591d3db2a559f598bf1
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-18 18:55:10 -0700

    Feat - Refactor road trip facade to allow for longer valid trips

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -2,9 +2,13 @@ class RoadTripFacade
   def self.new(origin, destination)
     time = MapQuestService.get_route_time(origin, destination)
     weather = ForecastFacade.new(destination)
-    if time
+    
+    if time && time[0..1].to_i <= 8 && time[0..1].to_f.ceil > 0
       temperature = weather[:hourly][time[0..1].to_i][:temp]
       conditions = weather[:hourly][time[0..1].to_i][:weather][0][:description]
+    elsif time
+      temperature = weather[:daily][time[0..1].to_i/24][:temp][:day]
+      conditions = weather[:daily][time[0..1].to_i/24][:weather][0][:description]
     else
       temperature = nil
       conditions = nil
