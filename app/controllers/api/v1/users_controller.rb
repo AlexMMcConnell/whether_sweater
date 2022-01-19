19f807b15214a89b80763dc68873f72e5ae6a4eb
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-18 18:57:26 -0700

    Refactor - Adjust user controller sad path to reflect lack of email

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
       render json: UserSerializer.new(User.find(user[:id])), status: 201
     elsif new_user[:password] != new_user[:password_confirmation]
       render json: "Passwords do not match", status: 400
-    elsif new_user[:email].nil?
+    elsif new_user[:email].empty?
       render json: "Email required to register", status: 400
     else
       render json: "Email already registered", status: 409
