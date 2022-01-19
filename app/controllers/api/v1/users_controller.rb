74a07e419e4d97a82cbb45eac904cc8784a5c00f
Alex McConnell <alexmmcconnell@gmail.com>
2022-01-18 18:55:28 -0700

    Refactor - Adjust if/else branching to account for sad paths

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::UsersController < ApplicationController
     new_user = request.POST
     user = User.new(email: new_user[:email], password: new_user[:password], api_key: ApiKey.new_key)
 
-    if user.save && response[:password] == response[:password_confirmation]
+    if user.save && new_user[:password] == new_user[:password_confirmation]
       render json: UserSerializer.new(User.find(user[:id])), status: 201
-    elsif response[:password] != response[:password_confirmation]
+    elsif new_user[:password] != new_user[:password_confirmation]
       render json: "Passwords do not match", status: 400
     elsif new_user[:email].nil?
       render json: "Email required to register", status: 400
